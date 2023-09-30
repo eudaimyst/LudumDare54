7c425ab4b9c5720c02ba18eca9b616e094c06887
skipgamer <tpilepich@gmail.com>
2023-09-30 15:24:45 +1000

    random letter positions

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@
 --
 -----------------------------------------------------------------------------------------
 
+local json = require("json")
+
 local sLen = string.len
 local sSub = string.sub
 ---@diagnostic disable-next-line: undefined-field --this is defined by solar2d math library not recognised
@@ -22,15 +24,72 @@ uiGroup.anchorChildren = true
 sceneGroup:insert(keyGroup)
 sceneGroup:insert(uiGroup)
 
+local roundedEdgeSize = 12
 local letters = "abcdefghijklmnopqrstuvwxyz"
-local letterTable = {}
-for i = 1, sLen(letters) do
-	letterTable[i] = sSub(letters, i, i)
-end
-local letterCount = #letterTable
 
-local layoutData = {}
+--tables
+local letterTable = {} --holds each defined letter
+local randomLetterTable = {} --holds letters from letterTable in random order
+local words = {} --loaded in loadWords(), check external/wordlist for attribution
+local layoutData = {} --stores data used for the layout of the keys
+local keys = {} --stores the key display objects
+
+--constants used for display
 local layoutMaxRadius, layoutRings = 200, 3
+local keySizeX, keySizeY = 50, 50
+local uiButtonHeight = 40
+local buttonOffset = 20
+local wordDisplayWidth = 300
+local submitButtonWidth = 130
+
+
+local function generateLetterTable(str)
+	local t = {}
+	for i = 1, sLen(str) do
+		t[i] = sSub(str, i, i)
+	end
+	--print("generated letter table: "..json.prettify(t))
+	return t
+end
+letterTable = generateLetterTable(letters)
+
+local function cleanTable(table) --removes nil values from a table
+	local t = {}
+	local count = 1
+	--print("cleaning table with length: "..#table)
+	for i = 1, #table do
+		if table[i] == "" then
+			--print("nil value in table at pos: "..i..", ignoring value")
+		else
+			--print("value: "..table[i].." at pos: "..i..", adding to new table")
+			t[count] = table[i]
+			count = count + 1
+		end
+	end
+	return t
+end
+
+local function makeRandomLetterTable(str)
+	local tempLetterTable = generateLetterTable(str)
+	local randomLetters = {}
+	for i = 1, sLen(str) do
+		--print(i)
+		if #tempLetterTable == 0 then
+			--print("tempLetterTable is empty, breaking")
+			break
+		end
+		local randomPos = mRand(1, #tempLetterTable)
+		--print("setting random letter table pos: "..i.." to: "..tempLetterTable[randomPos])
+		randomLetters[i] = tempLetterTable[randomPos]
+		tempLetterTable[randomPos] = ""
+		tempLetterTable = cleanTable(tempLetterTable)
+	end
+	return randomLetters
+end
+randomLetterTable = makeRandomLetterTable(letters)
+--print("random letter len: "..#randomLetterTable, json.prettify(randomLetterTable))
+
+
 local function layoutCalc() --calculates the layoutData table
 	local totalCircum = 0
 	for i = layoutRings, 1, -1 do --calculate data for the circles
@@ -43,7 +102,7 @@ local function layoutCalc() --calculates the layoutData table
 	for i = 1, #layoutData do --calculate letter count and angle for each ring
 		local ring = layoutData[i]
 		local percent = ring.circum / totalCircum
-		ring.letterCount = mRound(percent * letterCount)
+		ring.letterCount = mRound(percent * #letterTable)
 		ring.letterAngle = 360 / ring.letterCount
 		print("ring "..i.." percent: "..percent..", letterCount: "..ring.letterCount.. ", letterAngle: "..ring.letterAngle)
 	end
@@ -92,12 +151,9 @@ local function debugDraw() --for testing layout sizes and positions
 	drawCircles()
 end
 
-local keys = {}
-local keySizeX, keySizeY = 50, 50
-
-local function drawKeys() --draw display objects representing keys
+local function drawKeys(randomLetters) --draw display objects representing keys
 	local function drawKey(x, y, letter)
-		local button = display.newRect(keyGroup, 0, 0, keySizeX, keySizeY)
+		local button = display.newRoundedRect(keyGroup, 0, 0, keySizeX, keySizeY, roundedEdgeSize)
 		button:setFillColor(.3);
 		button.x = x
 		button.y = y
@@ -110,7 +166,13 @@ local function drawKeys() --draw display objects representing keys
 	for i = 1, #layoutData do
 		local ringData = layoutData[i]
 		for i2 = 1, #ringData.letters do
-			keys[count] = drawKey(ringData.letters[i2].x, ringData.letters[i2].y, letterTable[count])
+			local letter
+			if randomLetters then
+				letter = randomLetterTable[count]
+			else
+				letter = letterTable[count]
+			end
+			keys[count] = drawKey(ringData.letters[i2].x, ringData.letters[i2].y, letter)
 			count = count + 1
 		end
 	end
@@ -118,15 +180,12 @@ local function drawKeys() --draw display objects representing keys
 end
 
 local function drawUI()
-	local displayButtonHeight = 40
-	local buttonOffset = 20
-	local wordDisplayWidth = 300
-	local submitButtonWidth = 130
-	local wordDisplay = display.newRoundedRect(uiGroup,0,0,wordDisplayWidth,displayButtonHeight,12)
-	wordDisplay:setFillColor(.2)
-	wordDisplay.strokeWidth = 3
-	wordDisplay:setStrokeColor(1)
-	local submitButton = display.newRoundedRect(uiGroup,wordDisplayWidth/2 + submitButtonWidth/2 + buttonOffset,0,submitButtonWidth,displayButtonHeight,12)
+	local wordDisplayBox = display.newRoundedRect(uiGroup,0,0,wordDisplayWidth,uiButtonHeight,roundedEdgeSize)
+	wordDisplayBox:setFillColor(.1)
+	wordDisplayBox.strokeWidth = 3
+	wordDisplayBox:setStrokeColor(.9)
+	local submitButton = display.newRoundedRect(uiGroup,wordDisplayWidth/2 + submitButtonWidth/2 + buttonOffset,0,submitButtonWidth,uiButtonHeight,12)
+	submitButton:setFillColor(.9)
 	local submitButtonText = display.newText({ x = submitButton.x, y = submitButton.y, text = "submit", font = native.systemFont, fontSize = 18, align = "center" })
 	uiGroup:insert(submitButtonText)
 	submitButtonText:setFillColor(0)
@@ -134,7 +193,29 @@ local function drawUI()
 	uiGroup.y = keyGroup.y + keyGroup.height/2 + buttonOffset * 3
 end
 
+local function loadWords()
+	local path = system.pathForFile( "external/wordlist.txt", system.ResourceDirectory )
+	local file = io.open( path, "r" )
+	local count = 1
+	if file then
+		for line in file:lines() do
+			if count >= 310 then --ignore first 309 lines for attribution
+				words[#words+1] = line
+			end
+			count = count + 1
+		end
+		io.close( file )
+	else
+		print("no file")
+	end
+end
+
 layoutCalc()
 --debugDraw()
-drawKeys()
+drawKeys(true) --true for random letters, otherwise loads from letterTable in order
 drawUI()
+
+--load word list, we do this after drawing the sceen so not looking at nothing waiting to load
+
+loadWords()
+print("word count: "..#words)
