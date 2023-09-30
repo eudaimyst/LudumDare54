cdc63e5b2065e014cf7f478e456df1b63b89be9d
skipgamer <tpilepich@gmail.com>
2023-09-30 13:59:20 +1000

    key layout complete

--- main.lua
+++ main.lua
@@ -8,6 +8,11 @@
 
 local sLen = string.len
 local sSub = string.sub
+---@diagnostic disable-next-line: undefined-field --this is defined by solar2d math library not recognised
+local mRound = math.round
+local mRand = math.random
+local pi = math.pi
+print(pi)
 
 local sceneGroup = display.newGroup()
 local keyGroup = display.newGroup()
@@ -20,41 +25,94 @@ for i = 1, sLen(letters) do
 end
 local letterCount = #letterTable
 
-
-local keySizeX, keySizeY = 50, 50
 local layoutData = {}
 local layoutMaxRadius, layoutRings = 200, 3
-local keys = {}
-
-local _letterRow, _letter --recycled
-
 local function layoutCalc() --calculates the layoutData table
-	for i = layoutRings, 1, -1 do
+	local totalCircum = 0
+	for i = layoutRings, 1, -1 do --calculate data for the circles
 		local ring = {}
 		layoutData[#layoutData+1] = ring
 		ring.radius = layoutMaxRadius / layoutRings * i
+		ring.circum = 2 * pi * ring.radius
+		totalCircum = totalCircum + ring.circum
+	end
+	for i = 1, #layoutData do --calculate letter count and angle for each ring
+		local ring = layoutData[i]
+		local percent = ring.circum / totalCircum
+		ring.letterCount = mRound(percent * letterCount)
+		ring.letterAngle = 360 / ring.letterCount
+		print("ring "..i.." percent: "..percent..", letterCount: "..ring.letterCount.. ", letterAngle: "..ring.letterAngle)
+	end
+	for i = 1, #layoutData do --calculate letter positions
+		local ringData = layoutData[i]
+		ringData.letters = {}
+		local angleOffset = mRand(0, ringData.letterAngle)
+		for i2 = 1, ringData.letterCount do
+			local letter = {}
+			ringData.letters[i2] = letter
+			local angle = ringData.letterAngle * i2 + angleOffset
+			letter.x = ringData.radius * math.cos(math.rad(angle))
+			letter.y = ringData.radius * math.sin(math.rad(angle))
+		end
 	end
 end
 
-local function debugDraw()
+local function debugDraw() --for testing layout sizes and positions
 	local baseColor = { [1]= .2,[2]= .2,[3]= .2,[4]= .2 }
 	local debugGroup = display.newGroup()
-	sceneGroup:insert(debugGroup)
-	for i = 1, #layoutData do
-		local ringData = layoutData[i]
-		local circle = display.newCircle(debugGroup,0,0,ringData.radius);
-		local c = baseColor
-		c[2] = i * .3
-		circle:setFillColor( 0, 0, 0, 0);
-		circle.strokeWidth = 3
-		circle:setStrokeColor( c[1], c[2], c[3], c[4]);
+	local function drawCircles()
+		sceneGroup:insert(debugGroup)
+		for i = 1, #layoutData do
+			local ringData = layoutData[i]
+			local circle = display.newCircle(debugGroup,0,0,ringData.radius);
+			local c = baseColor
+			c[2] = i * .3
+			circle:setFillColor( 0, 0, 0, 0);
+			circle.strokeWidth = 3
+			circle:setStrokeColor( c[1], c[2], c[3], c[4]);
+		end
+		debugGroup.x, debugGroup.y = display.contentCenterX, display.contentCenterY
 	end
-	debugGroup.x, debugGroup.y = display.contentCenterX, display.contentCenterY
+
+	local function drawDotsForKeys()
+		for i = 1, #layoutData do
+			local ringData = layoutData[i]
+			for i2 = 1, #ringData.letters do
+				local letter = ringData.letters[i2]
+				local dot = display.newCircle(debugGroup, letter.x, letter.y, 5)
+				dot:setFillColor(1,0,0)
+			end
+		end
+	end
+	drawDotsForKeys()
+	drawCircles()
 end
 
+local keys = {}
+local keySizeX, keySizeY = 50, 50
+
 local function drawKeys() --draw display objects representing keys
+	local function drawKey(x, y, letter)
+		local button = display.newRect(keyGroup, 0, 0, keySizeX, keySizeY)
+		button:setFillColor(.3);
+		button.x = x
+		button.y = y
+		button.textRect = display.newText({ x = button.x, y = button.y, text = letter,	width = 50,	font = native.systemFont, fontSize = 18, align = "center" })
+		keyGroup:insert(button.textRect)
+		return button
+	end
+	
+	local count = 1
+	for i = 1, #layoutData do
+		local ringData = layoutData[i]
+		for i2 = 1, #ringData.letters do
+			keys[count] = drawKey(ringData.letters[i2].x, ringData.letters[i2].y, letterTable[count])
+			count = count + 1
+		end
+	end
+	keyGroup.x, keyGroup.y = display.contentCenterX, display.contentCenterY - display.contentCenterY / 5
 end
 
 layoutCalc()
-debugDraw()
+--debugDraw()
 drawKeys()
